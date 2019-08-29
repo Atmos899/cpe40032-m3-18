d579c581b7c4a6a94f0abe26ec79cba03bc4e775
LeMortz <52573794+LeMortz@users.noreply.github.com>
2019-08-29 20:46:15 +0800

    Add files via upload

---
+++ src/constants.lua
@@ -0,0 +1,27 @@
+--[[
+     CMPE40032 2018
+    Arkanoid Remake
+
+    -- constants --
+
+    Some global constants for our application.
+]]
+
+
+-- size of our actual window
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- size we're trying to emulate with push
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- paddle movement speed
+PADDLE_SPEED = 200
+
+POWERUP_GRAVITY = 20
+POWERUP_SPAWNING_ODDS = 1
+BALL_SPEED = 100
+MAX_KEY_TIME = 5
+MAX_ATTRACTOR_TIME = 10
+LOCKED_BRICK_SPAWNING_ODDS = 1
